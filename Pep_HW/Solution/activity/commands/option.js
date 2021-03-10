3db3a9f21b89f8c2481765bc3870c56b0d6a1bb2
Ravi Anand <43475033+ravianandfbg@users.noreply.github.com>
2021-03-10 14:06:19 +0530

    Add files via upload

---
+++ Pep_HW/Solution/activity/commands/option.js
@@ -0,0 +1,90 @@
+const fs = require("fs");
+const helpfnObj = require("./help");
+
+let optionChose = false;
+
+function options(input,filepath) {
+//input[0] -s ,input[1] -b ,....input[-1]-filepath
+    let data = fs.readFileSync(filepath, 'utf8');
+    for(let i=0; i<input.length-1; i++){
+        if (optionChose == true && (input[i] == "-b" || input[i] == "-n")){
+            return;
+        }
+        else if(input[i] == "-s"){
+            // console.log("------------------------(in -s)------------------------");
+            data = singBreak(data);
+        }
+        else if(input[i] == "-n" && optionChose == false){
+            // console.log("------------------------(in -n)------------------------");
+
+            optionChose = true;
+            data = numberingAll(data);
+        }
+        else if(input[i] == "-b" && optionChose == false){
+            // console.log("------------------------(in -b)------------------------");
+
+            optionChose = true;
+            data = numberingNonEmpty(data);
+        }
+        else{
+            if(input[i] != "-s"){
+                console.log("Wrong Input ! Please choose any of these options only :\n");
+                helpfnObj.helpfn(input);
+                // console.log(optionChose);
+
+                return;
+            }
+        }
+    }
+}
+
+function singBreak(data){
+    // console.log(typeof(data));
+    // return;
+    data = data.replace(/\n\s*\n/g, '\n\n');
+    console.log(data);
+    return data;
+}
+
+function numberingAll(data){
+    const lines = data.split(/\r?\n/);
+    // console.log(lines);
+    
+    let string_data = "";
+    let count = 0;
+    for(let i=0; i<lines.length; i++){
+        count+=1;
+        console.log(count, ". ", lines[i]);
+
+        string_data += (count + ". " + lines[i] + "\n");
+    }
+    // console.log(string_data);
+    return string_data;
+}
+
+function numberingNonEmpty(data){
+    let lines = data.split(/\r?\n/);
+    // console.log(lines);
+
+    let string_data = "";
+    let count = 0;
+    for(let i=0; i<lines.length; i++){
+        let line = lines[i];
+        
+        if (line == ""){
+            count++;
+            console.log(count, ". ", line);
+            string_data += (count + ". " + line + "\n")
+        }
+        else{
+            console.log(line);
+            string_data += (line + "\n");
+        }
+    }
+
+    return string_data;
+}
+
+module.exports = {
+    optionFn:options,
+}
---
+++ Pep_HW/Solution/activity/wcat.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+const helpfnObj = require("./commands/help");
+const optionfnObj = require("./commands/option");
+const viewfnObj = require("./commands/view");
+
+
+
+let input = process.argv.slice(2); 
+let filepath = input[input.length-1];
+
+function isFile(dirpath){
+    return (dirpath!=path.basename(dirpath));
+}
+
+//input[0]-filepath
+if(input.length==1){
+    viewfnObj.viewfn(input);
+}
+else if (input.length>1){
+    //1.input[0]-filepath input[-1]-filepath
+    let isFilebool = isFile(input[0]);
+    if(isFilebool==true){
+        viewfnObj.viewfn(input);
+    }
+    else{
+    //2.input[0]-option input[-1]-filepath
+        if(!fs.existsSync(filepath)){
+            console.log("File does not exist!!");
+        }
+        else{
+            optionfnObj.optionFn(input,filepath);
+        }
+    }
+}
